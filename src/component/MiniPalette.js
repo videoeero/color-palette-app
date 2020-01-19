ca91a1c2c7e255aeb4c5781fa22c77930ec7a3b8
Eero Suvanto <ejsuva@utu.fi>
2020-01-19 20:59:48 +0200

    Links from home to minipalettes

--- src/component/MiniPalette.js
+++ src/component/MiniPalette.js
@@ -36,7 +36,7 @@ const styles = {
     display: 'inline-block',
     margin: '0 auto',
     position: 'relative',
-    marginBottom: '-3.5px'
+    marginBottom: '-2.2px'
   }
 };
 
@@ -49,9 +49,9 @@ function MiniPalette(props) {
       key={color.name}
     />
   ));
-  console.log(classes);
+
   return (
-    <div className={classes.root}>
+    <div className={classes.root} onClick={props.handleClick}>
       <div className={classes.colors}>{miniColorBoxes}</div>
       <h5 className={classes.title}>
         {paletteName} <span className={classes.emoji}>{emoji}</span>
